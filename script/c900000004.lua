b17b631b6db48e5c48cba9a89ac7fc12f909eced
The-Mysterious-Man <102930916+The-Mysterious-Man@users.noreply.github.com>
2022-04-08 22:11:41 -0300

    Add files via upload

---
+++ script/c900000004.lua
@@ -0,0 +1,122 @@
+--Millennium Stone
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--ATTRIBUTE
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_MZONE+LOCATION_GRAVE,LOCATION_MZONE+LOCATION_GRAVE)
+	e2:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+	e2:SetValue(ATTRIBUTE_DARK)
+	e2:SetTarget(s.tg)
+	c:RegisterEffect(e2)
+	--Cannot be destroyed
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e3:SetCountLimit(1)
+	e3:SetValue(s.valcon)
+	c:RegisterEffect(e3)
+	--Banish
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e4:SetType(EFFECT_TYPE_ACTIVATE)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetCost(s.cost)
+	e4:SetTarget(s.target)
+	e4:SetOperation(s.activate)
+	c:RegisterEffect(e4)
+	--activate
+	local e5=Effect.CreateEffect(c)
+	e5:SetCategory(CATEGORY_RELEASE+CATEGORY_SPECIAL_SUMMON)
+	e5:SetType(EFFECT_TYPE_ACTIVATE)
+	e5:SetCode(EVENT_FREE_CHAIN)
+	e5:SetTarget(s.target)
+	e5:SetOperation(s.activate)
+	c:RegisterEffect(e5)
+end
+s.listed_names={900000000,900000002,900000003}
+function s.tg(e,c)
+	if c:GetFlagEffect(1)==0 then
+		c:RegisterFlagEffect(1,0,0,0)
+		local eff
+		if c:IsLocation(LOCATION_MZONE) then
+			eff={Duel.GetPlayerEffect(c:GetControler(),EFFECT_NECRO_VALLEY)}
+		else
+			eff={c:GetCardEffect(EFFECT_NECRO_VALLEY)}
+		end
+		c:ResetFlagEffect(1)
+		for _,te in ipairs(eff) do
+			local op=te:GetOperation()
+			if not op or op(e,c) then return false end
+		end
+	end
+	return true
+end
+function s.valcon(e,re,r,rp)
+	return (r&REASON_EFFECT)~=0
+end
+function s.cfilter(c)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,2,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local rg=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,2,2,nil)
+	Duel.Remove(rg,POS_FACEUP,REASON_COST)
+end
+function s.filter(c)
+	return c:IsCode(900000002) and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.eqfilter(c)
+	return c:IsCode(900000001) and c:IsCode(900000002)
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsCode(900000000) and c:IsReleasableByEffect()
+		and c:GetEquipGroup():FilterCount(s.eqfilter,nil)>0
+end
+function s.filter(c,e,tp)
+	return c:IsCode(900000003) and c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local loc=LOCATION_MZONE
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then loc=0 end
+		return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,loc,1,nil)
+			and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_RELEASE,nil,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local loc=LOCATION_MZONE
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then loc=0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE,loc,1,1,nil)
+	if #g>0 and Duel.Release(g,REASON_EFFECT)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+		if #sg>0 then
+			Duel.SpecialSummon(sg,0,tp,tp,true,false,POS_FACEUP)
+		end
+	end
+end
