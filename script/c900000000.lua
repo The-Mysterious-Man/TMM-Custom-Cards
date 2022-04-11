80a053e923d1d8e296a17969d3290164a598ad31
The-Mysterious-Man <102930916+The-Mysterious-Man@users.noreply.github.com>
2022-04-11 20:46:24 -0300

    Add files via upload

--- script/c900000000.lua
+++ script/c900000000.lua
@@ -1,4 +1,4 @@
--- Miracle of the Mystical Elf
+-- Wrath of the Mystical Elf
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -6,7 +6,7 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--Cannot Special Summon
+	--Cannot Sp. Summon
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetRange(LOCATION_SZONE)
@@ -15,111 +15,76 @@ function s.initial_effect(c)
 	e2:SetTargetRange(1,0)
 	e2:SetTarget(s.splimit)
 	c:RegisterEffect(e2)
-	--immune
+	--Cannot be destroyed by card effects while you control a normal monster
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetCode(EFFECT_IMMUNE_EFFECT)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e3:SetRange(LOCATION_SZONE)
-	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e3:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_NORMAL))
-	e3:SetValue(s.immfilter)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT+EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e3:SetCondition(s.incon)
+	e3:SetValue(1)
 	c:RegisterEffect(e3)
-	--Atk
+	--atk/def up
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_FIELD)
 	e4:SetCode(EFFECT_UPDATE_ATTACK)
 	e4:SetRange(LOCATION_SZONE)
-	e4:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e4:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_NORMAL))
-	e4:SetValue(800)
+	e4:SetTargetRange(LOCATION_MZONE,0)
+	e4:SetTarget(s.tg)
+	e4:SetValue(s.val)
 	c:RegisterEffect(e4)
-	--Def
 	local e5=e4:Clone()
 	e5:SetCode(EFFECT_UPDATE_DEFENSE)
 	c:RegisterEffect(e5)
-	--decrease tribute
+	--immune
 	local e6=Effect.CreateEffect(c)
-	e6:SetDescription(aux.Stringid(id,0))
 	e6:SetType(EFFECT_TYPE_FIELD)
-	e6:SetCode(EFFECT_SUMMON_PROC)
+	e6:SetCode(EFFECT_IMMUNE_EFFECT)
 	e6:SetRange(LOCATION_SZONE)
-	e6:SetTargetRange(LOCATION_HAND,0)
-	e6:SetCountLimit(1)
-	e6:SetCondition(s.ntcon)
-	e6:SetTarget(aux.FieldSummonProcTg(s.nttg))
+	e6:SetTargetRange(LOCATION_MZONE,0)
+	e6:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_NORMAL))
+	e6:SetValue(s.efilter)
 	c:RegisterEffect(e6)
-	--Draw 2
+	--Activate
 	local e7=Effect.CreateEffect(c)
-	e7:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
-	e7:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e7:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e7:SetType(EFFECT_TYPE_IGNITION)
 	e7:SetCode(EVENT_FREE_CHAIN)
+	e7:SetRange(LOCATION_SZONE)
 	e7:SetTarget(s.target)
 	e7:SetOperation(s.activate)
 	c:RegisterEffect(e7)
-	--Special summon from deck
-	local e8=Effect.CreateEffect(c)
-	e8:SetDescription(aux.Stringid(id,1))
-	e8:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e8:SetType(EFFECT_TYPE_IGNITION)
-	e8:SetRange(LOCATION_GRAVE)
-	e8:SetCost(aux.bfgcost)
-	e8:SetTarget(s.sptg)
-	e8:SetOperation(s.spop)
-	c:RegisterEffect(e8)
 end
 function s.splimit(e,c,tp,sumtp,sumpos)
 	return c:GetType()~=TYPE_NORMAL
 end
-function s.filter1(c,e,tp)
-	return c:IsType(TYPE_NORMAL) 
+function s.incon(e)
+	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsType,TYPE_NORMAL),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
 end
-function s.immfilter(e,te)
-	return te:IsActiveType(TYPE_MONSTER)
-end
-function s.ntcon(e,c,minc)
-	if c==nil then return true end
-	return minc==0 and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
-end
-function s.nttg(e,c)
-	return c:IsLevelAbove(5) and c:IsType(TYPE_NORMAL)
+function s.tg(e,c)
+	return c:IsType(TYPE_NORMAL)
 end
 function s.filter(c)
-	return c:IsType(TYPE_MONSTER) and c:IsType(TYPE_NORMAL) and c:IsAbleToDeck()
+	return c:IsFaceup() and c:IsType(TYPE_NORMAL)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter(chkc) end
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,2)
-		and Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,5,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,5,5,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,#g,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+function s.val(e,c)
+	return Duel.GetMatchingGroupCount(s.filter,c:GetControler(),LOCATION_MZONE,0,nil)*300
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-	if not tg or tg:FilterCount(Card.IsRelateToEffect,nil,e)~=5 then return end
-	Duel.SendtoDeck(tg,nil,0,REASON_EFFECT)
-	local g=Duel.GetOperatedGroup()
-	if g:IsExists(Card.IsLocation,1,nil,LOCATION_DECK) then Duel.ShuffleDeck(tp) end
-	local ct=g:FilterCount(Card.IsLocation,nil,LOCATION_DECK+LOCATION_EXTRA)
-	if ct==5 then
-		Duel.BreakEffect()
-		Duel.Draw(tp,2,REASON_EFFECT)
-	end
+function s.efilter(e,te)
+	return te:GetOwnerPlayer()~=e:GetHandlerPlayer() and te:IsActiveType(TYPE_MONSTER)
 end
-function s.spfilter(c,e,tp)
+function s.filter2(c,e,tp)
 	return c:IsType(TYPE_NORMAL) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+		and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
