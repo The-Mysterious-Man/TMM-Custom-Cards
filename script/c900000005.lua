787d3b816163a64cf43253f8375178579e516e43
The-Mysterious-Man <102930916+The-Mysterious-Man@users.noreply.github.com>
2022-04-18 16:31:17 -0300

    Add files via upload

--- script/c900000005.lua
+++ script/c900000005.lua
@@ -2,40 +2,65 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTargetRange(LOCATION_MZONE+LOCATION_SZONE,0)
-	e1:SetTarget(s.disable)
-	e1:SetCode(EFFECT_DISABLE)
-	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	local e1=Fusion.CreateSummonEff(c,nil,s.fextra,nil,nil,nil)
 	e1:SetCost(s.cost)
-	e1:SetOperation(s.activate)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	c:RegisterEffect(e1)
-end
-function s.disable(e,c)
-	return c:IsType(TYPE_MONSTER) and c:IsType(TYPE_SPELL) and c:IsType(TYPE_TRAP) and not c:IsCode(id)
-end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	Fusion.RegisterSummonEff{handler=c,s.fextra}
+	if not AshBlossomTable then AshBlossomTable={} end
+	table.insert(AshBlossomTable,e1)
+	Duel.AddCustomActivityCounter(id,ACTIVITY_SPSUMMON,s.counterfilter)
+	--fusion summon
+	local params = {c,s.matfilter,s.fextra2,Fusion.BanishMaterial}
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCost(aux.bfgcost)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e2:SetTarget(Fusion.SummonEffTG(table.unpack(params)))
+	e2:SetOperation(Fusion.SummonEffOP(table.unpack(params)))
+	c:RegisterEffect(e2)
+	if not GhostBelleTable then GhostBelleTable={} end
+	table.insert(GhostBelleTable,e2)
 end
 function s.fextra(e,tp,mg)
-	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToGrave),tp,LOCATION_HAND+LOCATION_DECK,0,nil)
+	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToGrave),tp,LOCATION_DECK,0,nil)
+end
+function s.counterfilter(c)
+	return c:IsType(TYPE_MONSTER)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0 end
-	local e1=Effect.CreateEffect(e:GetHandler())
+	local c=e:GetHandler()
+	--Cannot Special Summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e1:SetReset(RESET_PHASE+PHASE_END)
 	e1:SetTargetRange(1,0)
-	e1:SetLabelObject(e)
 	e1:SetTarget(s.splimit)
+	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
-	aux.RegisterClientHint(e:GetHandler(),nil,tp,1,0,aux.Stringid(id,1),nil)
+	--Clock Lizard check
+	aux.addTempLizardCheck(c,tp,s.lizfilter)
 end
 function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
-	return not c:IsType(TYPE_FUSION)
+	return c:IsType(TYPE_MONSTER)
+end
+function s.lizfilter(e,c)
+	return not c:IsOriginalType(TYPE_FUSION)
+end
+function s.matfilter(c)
+	return aux.SpElimFilter(c) and c:IsAbleToRemove()
+end
+function s.extrafil(c)
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToRemove()
+end
+function s.fextra2(e,tp,mg)
+	if not Duel.IsPlayerAffectedByEffect(tp,69832741) then
+		return Duel.GetMatchingGroup(s.extrafil,tp,LOCATION_GRAVE,0,nil)
+	end
+	return nil
 end
